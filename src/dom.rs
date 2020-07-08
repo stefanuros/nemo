d0d0dc824f73f6bc9f0eab22c11784b80a807c25
Stefan Urosevic <stefanuros@gmail.com>
2020-07-07 23:24:04 -0400

    Part 1: Excercise 3

--- src/dom.rs
+++ src/dom.rs
@@ -1,9 +1,26 @@
+use std::collections::HashMap;
+
 type AttrMap = HashMap<String, String>;
 
-struct Node {
+
+// 
+// Node
+// 
+
+pub struct Node {
   children: Vec<Node>,
-  nodeType: NodeType,
+  node_type: NodeType,
 }
+
+impl Node {
+  fn get_node_name(&self) -> String {
+    return self.node_type.get_node_name()
+  }
+}
+
+// 
+// Node Type
+// 
 
 enum NodeType {
   Element(ElementNode),
@@ -17,34 +34,85 @@ enum NodeType {
   // DocumentFragment(DocumentFragmentNode),
 }
 
+impl NodeType {
+  fn get_node_name(&self) -> String {
+    match *self {
+      NodeType::Element(ref e) => return e.get_node_name(),
+      NodeType::Text(ref t) => return t.get_node_name(),
+      NodeType::Comment(ref c) => return c.get_node_name(),
+    }
+  }
+}
+
+// 
+// Element Node
+// 
+
 struct ElementNode {
-  tagName: String,
+  tag_name: String,
   attributes: AttrMap,
 }
+
+impl ElementNode {
+  fn get_node_name(&self) -> String {
+    return self.tag_name.to_string();
+  }
+}
+
+// 
+// Text Node
+// 
 
 struct TextNode {
   text: String,
 }
 
+impl TextNode {
+  fn get_node_name(&self) -> String {
+    return "#text".to_string();
+  }
+}
+
+// 
+// Comment Node
+// 
+
 struct CommentNode {
   data: String,
 }
 
-fn text(data: String) -> Node {
+impl CommentNode {
+  fn get_node_name(&self) -> String {
+    return "#comment".to_string();
+  }
+}
+
+pub fn text(data: &str) -> Node {
   return Node { 
     children: Vec::new(),
-    nodeType: NodeType::Text(TextNode {
-      text: data,
+    node_type: NodeType::Text(TextNode {
+      text: data.to_string(),
     }),
   }
 }
 
-fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+pub fn elem(name: &str, attrs: AttrMap, children: Vec<Node>) -> Node {
   Node {
     children: children,
     node_type: NodeType::Element(ElementNode {
-        tagName: name,
+        tag_name: name.to_string(),
         attributes: attrs,
     }),
+  }
+}
+
+pub fn pretty_print_dom_tree(root: Node, level: usize) {
+  let indent = level * 4;
+  // Print the name based on the 
+  print!("{:indent$}{}\n", "", root.get_node_name(), indent=indent);
+
+  // Loop through the children
+  for child in root.children {
+    pretty_print_dom_tree(child, indent + 1);
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::collections::HashMap;
+
+pub mod dom;
+
 fn main() {
+
+  let text = dom::text("text");
+  let elem = dom::elem("elem", HashMap::new(), vec![text]);
+
   println!("Hello, world!");
+
+  dom::pretty_print_dom_tree(elem, 0);
+
 }
 
