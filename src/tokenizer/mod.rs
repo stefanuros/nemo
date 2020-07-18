d52f928548ed9d09ec91e5479b8a439cff7a64a5
Stefan Urosevic <stefanuros@gmail.com>
2020-07-18 19:56:04 -0400

    Added tag name state handler

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -94,6 +94,7 @@ fn tokenize(
     DataState::PLAINTEXTState => state_transitions::plaintext_state_transition(c),
     DataState::TagOpenState => state_transitions::tag_open_state_transition(c, current_state, create_token),
     DataState::EndTagOpenState => state_transitions::end_tag_open_state_transition(c, current_state, create_token),
+    DataState::TagNameState => state_transitions::tag_name_state_transition(c, current_state, create_token),
     _ => (None, false),
   }
 }
--- src/tokenizer/state_transitions/mod.rs
+++ src/tokenizer/state_transitions/mod.rs
@@ -4,6 +4,7 @@ mod plaintext_state_transition;
 mod rawtext_state_transition;
 mod rcdata_state_transition;
 mod script_data_state_transition;
+mod tag_name_state_transition;
 mod tag_open_state_transition;
 
 pub use data_state_transition::data_state_transition;
@@ -12,4 +13,5 @@ pub use plaintext_state_transition::plaintext_state_transition;
 pub use rawtext_state_transition::rawtext_state_transition;
 pub use rcdata_state_transition::rcdata_state_transition;
 pub use script_data_state_transition::script_data_state_transition;
+pub use tag_name_state_transition::tag_name_state_transition;
 pub use tag_open_state_transition::tag_open_state_transition;
