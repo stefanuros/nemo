5b462b1ff039007ed3b882ab7664a50d7597ee88
Stefan Urosevic <stefanuros@gmail.com>
2020-07-18 00:34:23 -0400

    Added tag open state handler

--- src/errors/tokenizer_errors/unexpected_null_character_parse_error.rs
+++ src/errors/tokenizer_errors/unexpected_null_character_parse_error.rs
@@ -1,5 +1,4 @@
 pub fn error(state: String, c: char) {
-  // TODO Move this to static assets file
   const ERROR_CODE: &str = "unexpected-null-character";
   let error_state = format!("state: {}, char: {}", state, c.escape_unicode());
   const ERROR_MESSAGE: &str = "This error occurs if the parser encounters a U+0000 NULL code point in the input stream in certain positions. In general, such code points are either completely ignored or, for security reasons, replaced with a U+FFFD REPLACEMENT CHARACTER.";
