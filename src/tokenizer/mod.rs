68464db4edbd3e2dc6f685ccbda8d48ebc5102bc
Stefan Urosevic <stefanuros@gmail.com>
2020-07-29 22:24:11 -0400

    Added comment less than sign bang dash state handler

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -143,6 +143,7 @@ fn tokenize(
     DataState::CommentState => state_transitions::comment_state_transition(c, current_state, current_token),
     DataState::CommentLessThanSignState => state_transitions::comment_less_than_sign_state_transition(c, current_state, current_token),
     DataState::CommentLessThanSignBangState => state_transitions::comment_less_than_sign_bang_state_transition(c, current_state),
+    DataState::CommentLessThanSignBangDashState => state_transitions::comment_less_than_sign_bang_dash_state_transition(c, current_state),
     _ => (None, false),
   }
 }
