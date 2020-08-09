912fc235c4f0dcf44081fc106b27f96f82f3c0ee
Stefan Urosevic <stefanuros@gmail.com>
2020-08-09 02:20:31 -0400

    Added numeric character references

--- src/types/tokenizer_types/mod.rs
+++ src/types/tokenizer_types/mod.rs
@@ -3,4 +3,5 @@ pub mod data_states;
 pub mod token_types;
 
 mod constants;
-pub use constants::NAMED_CHARACTER_REFERENCES;
+pub use constants::NAMED_CHARACTER_REFERENCE;
+pub use constants::NUMERIC_CHARACTER_REFERENCE;
